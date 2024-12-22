d7a5546b3fb5eb42ea3a481056e3fa8ab0c99be1
Gurvir <gurvirrandhawa28@gmail.com>
2024-12-22 16:15:16 -0500

    Cloud icons added for all platforms !

--- src/App.tsx
+++ src/App.tsx
@@ -14,17 +14,17 @@ const [greetMsg, setGreetMsg] = useState("");
 
     return (
         <main className="container">
-            <h1>Welcome to Tauri + React</h1>
+            <h1>Cloud, the lightweight rich-text editor.</h1>
 
             <div className="row">
                 <a href="https://vitejs.dev" target="_blank">
-                    <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+                    <img src="/Cloud.png" className="logo vite" alt="Vite logo" />
                 </a>
                 <a href="https://tauri.app" target="_blank">
-                    <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+                    <img src="/Cloud.png" className="logo tauri" alt="Tauri logo" />
                 </a>
                 <a href="https://reactjs.org" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
+                    <img src="/Cloud.png" className="logo react" alt="React logo" />
                 </a>
             </div>
             <p>Click on the Tauri, Vite, and React logos to learn more.</p>
